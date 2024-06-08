87351be325a96ab8aecbf23d6b2af5aec230a92e
josephesteva <88625145+josephesteva@users.noreply.github.com>
2024-06-08 11:53:53 -0400

    Implement palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringToReverse = string.replace(/[^\w\s\']|_/g, "").replace(/ /g, '').toLowerCase();
+    const reverseStringArray = [];
+    for (i=0; i < stringToReverse.length; i++) {
+        reverseStringArray.unshift(stringToReverse[i])
+    }
+    const reverseString = reverseStringArray.join('');
+    return stringToReverse === reverseString
 };
 
 // Do not edit below this line
