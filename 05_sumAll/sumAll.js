b3a35134876df248464fb5d40d5dec562cb332c0
josephesteva <88625145+josephesteva@users.noreply.github.com>
2024-06-07 22:35:15 -0400

    Implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    console.log(typeof num1);
+    if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR"
+    }
+    return ((num1+num2)/2 * (Math.abs(num2-num1) + 1))
 };
 
 // Do not edit below this line
