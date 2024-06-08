89ced9293ebc837de16a958d3b5cc66b17a1bccd
josephesteva <88625145+josephesteva@users.noreply.github.com>
2024-06-08 11:24:55 -0400

    Implement calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0
+  for (i=0; i < array.length; i++) {
+    sum = sum + array[i]
+  }
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  if (array.length < 1) {
+    return 0
+  }
+	let total = 1
+  for (i=0; i < array.length; i++) {
+    total = total * array[i]
+  }
+  return total
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	total = 1
+  for (i=a; i > 0; i--) {
+    total = total * i
+  }
+  return total
 };
 
 // Do not edit below this line
