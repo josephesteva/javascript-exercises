c5223a90ba35cbf22fb0e9323015cf0bdbd35147
josephesteva <88625145+josephesteva@users.noreply.github.com>
2024-06-07 22:16:50 -0400

    Implement reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reverseArray = []
+    for (i=0; i < string.length; i++) {
+        reverseArray.unshift(string[i])
+    }
+    return reverseArray.join('')
 };
 
 // Do not edit below this line
