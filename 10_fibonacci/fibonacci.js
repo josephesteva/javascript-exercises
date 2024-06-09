34830d568490ea23d88ff519b573eac276971285
josephesteva <88625145+josephesteva@users.noreply.github.com>
2024-06-08 21:23:14 -0400

    Implement fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let total = 1;
+    let olderPrior = 0 
+    let mostRecent = 1;
+    for (i = 1; i < +number; i++) {
+        total = mostRecent + olderPrior
+        console.log(total);
+        olderPrior = mostRecent
+        mostRecent = total
+    }
+    if (number == 0) return 0;
+    if (number < 0) {
+        return "OOPS"
+    }
+    return total
 };
 
 // Do not edit below this line
