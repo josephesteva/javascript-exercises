916740d189edd05e788ce5680a9f3fae8dcea66c
josephesteva <88625145+josephesteva@users.noreply.github.com>
2024-06-11 08:47:10 -0400

    Implement getTheTitles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    return array.map(book => book.title)
 
+    // ALTERNATE method using loop
+    // const titleArray =[];
+    // for (i=0; i< array.length; i++) {
+    //     titleArray.push(array[i].title)
+    // }
+    // return titleArray
 };
 
 // Do not edit below this line
