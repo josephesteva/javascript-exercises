39f4b4f656a73a5dca15e411e1092d499d4c34ed
josephesteva <88625145+josephesteva@users.noreply.github.com>
2024-06-11 11:17:24 -0400

    Implement removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let returnArray = [...array]
+    for (i=1; i < arguments.length; i++) {
+        const filteredArray = returnArray.filter((item) => item !== arguments[i])
+        returnArray = filteredArray
+    }
+    return returnArray
 };
 
 // Do not edit below this line
